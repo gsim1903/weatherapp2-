0aab40dec6ac984cdf80aa74edd5a097a7e19395
gsim1903 <gavin@internetbusker.co>
2022-01-15 16:17:32 +0000

    app.js code for App class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+import React, { Component } from 'react'
+
+class App extends component {
+  state = {
+    geolocation: {}
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(position => {
+      //debugger
+      this.setState({ geolocation: position.coords })
+    })
+  }
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+
 }
 
-export default App
+export default App 
