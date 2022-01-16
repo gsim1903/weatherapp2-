39555d9787c3de620aabd00da2316afa5d45197e
gsim1903 <gavin@internetbusker.co>
2022-01-16 08:13:37 +0000

    update for geolocation. remove keys.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 
+
 class App extends Component {
   state = {
     geolocation: {}
@@ -8,17 +9,25 @@ class App extends Component {
 
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(async position => {
+
+      const openCageKey = process.env.REACT_APP_KEY_OPENCAGE;
+      const openWeatherMapKey = process.env.REACT_APP_WEATHERKEY;
+      
+
       let { latitude, longitude } = position.coords
-      let locationResponse = await axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=`)
-      let weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=`)
+
+      let locationResponse = await axios.get
+      (`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${openCageKey}`)
+      let weatherResponse = await axios.get
+      (`https://api.openweathermap.org/data/2.5/onecall?&lat=${latitude}&lon=${longitude}&units=metric&appid=${openWeatherMapKey}`)
 
       let weatherInfo = {
-        city: locationResponse.data.results[0].components.postal_city,
+        city: locationResponse.data.results[0].components.city,
         temp: weatherResponse.data.current.temp
       }
 
       this.setState({ location: weatherInfo })
-      //debugger
+      debugger
     })
   }
   render() {
