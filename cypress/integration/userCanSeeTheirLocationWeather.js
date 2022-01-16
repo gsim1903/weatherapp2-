8ab4c3e3200f6835c698b6b467204755c4af5188
gsim1903 <gavin@internetbusker.co>
2022-01-16 11:51:30 +0000

    Update cypress for fixtures. Create simple display

--- cypress/integration/userCanSeeTheirLocationWeather.js
+++ cypress/integration/userCanSeeTheirLocationWeather.js
@@ -1,18 +1,31 @@
-describe('weather info for a users lcoation', () => {
-    cy.visit('/', ({
-        onBeforeload(window) {
-          const stubLocation = {
-            coords: { 
-                latitiude: 57.7308, 
-                longitute: 11.9834
-             },
-        }
+describe("weather info for user`s location", () => {
+  beforeEach(() => {
+    cy.intercept("https://api.openweathermap.org/data/2.5/**", {
+      fixture: "weather.json",
+    });
+    cy.intercept("https://api.opencagedata.com/geocode/v1/json**", {
+      fixture: "location.json",
+    });
+  });
+  it("is expected to be displayed on initial render", () => {
+    cy.visit("/", {
+      onBeforeLoad(window) {
+        const stubLocation = {
+          coords: {
+            latitude: 59.46081,
+            longitude: 17.88477,
+          },
+        };
         cy.stub(window.navigator.geolocation, "getCurrentPosition").callsFake(
-            (callback) => {
-              return callback(stubLocation)
-            }
-        )
-        }
-    }))
-})
-    
+          (callback) => {
+            return callback(stubLocation);
+          }
+        );
+      },
+    });
+    cy.get("[data-cy=weather-display]").within(() => {
+      cy.get("[data-cy=temp]").should("contain", "5.53°C");
+      cy.get("[data-cy=city]").should("contain", "Edinburgh");
+    });
+  });
+});
--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import {Menu, Grid, Header } from 'semantic-ui-react'
 
 
 class App extends Component {
   state = {
-    geolocation: {}
+    geolocation: {},
+    location: {}
   }
 
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(async position => {
 
-      const openCageKey = process.env.REACT_APP_KEY_OPENCAGE;
-      const openWeatherMapKey = process.env.REACT_APP_WEATHERKEY;
-      
+      const openCageKey = process.env.REACT_APP_KEY_OPENCAGE
+      const openWeatherMapKey = process.env.REACT_APP_WEATHERKEY
+
 
       let { latitude, longitude } = position.coords
 
       let locationResponse = await axios.get
-      (`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${openCageKey}`)
+        (`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${openCageKey}`)
       let weatherResponse = await axios.get
-      (`https://api.openweathermap.org/data/2.5/onecall?&lat=${latitude}&lon=${longitude}&units=metric&appid=${openWeatherMapKey}`)
+        (`https://api.openweathermap.org/data/2.5/onecall?&lat=${latitude}&lon=${longitude}&units=metric&appid=${openWeatherMapKey}`)
 
       let weatherInfo = {
         city: locationResponse.data.results[0].components.city,
@@ -27,14 +29,24 @@ class App extends Component {
       }
 
       this.setState({ location: weatherInfo })
-      debugger
+      
     })
   }
-  render() {
-    return (
-      <div>
 
-      </div>
+  render() {
+    const temp = this.state.location.temp
+    const city = this.state.location.city
+
+    return (
+      <Menu>
+        <Grid.Column data-cy="weather-display">
+          <Header>The local weather in </Header>
+          <p data-cy="city">{city} is </p>
+          <p data-cy="temp">{temp}°C </p>
+          <p>
+          </p>
+        </Grid.Column>
+      </Menu>
     )
   }
 
